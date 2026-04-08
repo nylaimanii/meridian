06d2b966455cf3700b35c5e54ca8a359e78a9037
nylaimanii <nylaimanii7@gmail.com>
2026-04-08 12:30:05 -0400

    fix: add css-loader/style-loader to rspack config, clear empty main.css stub

--- rspack.config.js
+++ rspack.config.js
@@ -1,24 +1,17 @@
 const { defineConfig } = require("@meteorjs/rspack");
 
-/**
- * Rspack configuration for Meteor projects.
- *
- * Provides typed flags on the `Meteor` object, such as:
- * - `Meteor.isClient` / `Meteor.isServer`
- * - `Meteor.isDevelopment` / `Meteor.isProduction`
- * - …and other flags available
- *
- * Use these flags to adjust your build settings based on environment.
- */
 module.exports = defineConfig((Meteor) => {
   return {
     module: {
       rules: [
-        // Add support for importing SVGs as React components
         {
           test: /\.svg$/i,
           issuer: /\.[jt]sx?$/,
           use: ["@svgr/webpack"],
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
         },
       ],
     },
